5c9a1b653751b03998a9247b06fe137a4cf25f46
AnOwlbear <anowlbear@protonmail.com>
2023-12-12 22:37:54 +0000

    completed day two part two

--- src/main/kotlin/day2.kt
+++ src/main/kotlin/day2.kt
@@ -4,19 +4,39 @@ const val max_red = 12
 const val max_green = 13
 const val max_blue = 14
 
-data class Game(val id: Int, val draws: List<Draw>)
-data class Draw(val red: Int, val green: Int, val blue: Int)
+data class Game(val id: Int, val draws: List<BallSet>)
+data class BallSet(var red: Int, var green: Int, var blue: Int)
 
 fun main() {
     val input = object {}.javaClass.getResourceAsStream("/day2.txt")?.bufferedReader()?.readLines()
         ?: throw RuntimeException("Invalid input file")
 
-    val result = input
+    println(part1(input))
+    println(part2(input))
+}
+
+fun part2(input: List<String>): Int {
+    return input
+        .map { line -> parseGame(line) }
+        .map { game -> calcMinBallSet(game) }
+        .sumOf { b -> b.red * b.blue * b.green }
+}
+
+fun calcMinBallSet(game: Game): BallSet {
+    val maxValues = BallSet(0, 0, 0)
+    for (draw in game.draws) {
+        if (draw.red > maxValues.red) maxValues.red = draw.red
+        if (draw.green > maxValues.green) maxValues.green = draw.green
+        if (draw.blue > maxValues.blue) maxValues.blue = draw.blue
+    }
+    return maxValues
+}
+
+fun part1(input: List<String>): Int {
+    return input
         .map { line -> parseGame(line) }
         .filter { game -> validateGame(game) }
         .sumOf { game -> game.id }
-
-    println(result)
 }
 
 fun validateGame(game: Game): Boolean {
@@ -25,7 +45,7 @@ fun validateGame(game: Game): Boolean {
             return false
     }
 
-    return true;
+    return true
 }
 
 fun parseGame(line: String): Game {
@@ -36,7 +56,7 @@ fun parseGame(line: String): Game {
     return Game(id.toInt(), draws)
 }
 
-fun parseDraw(drawString: String): Draw {
+fun parseDraw(drawString: String): BallSet {
     val colours = mutableMapOf("red" to 0, "blue" to 0, "green" to 0)
     for (segment in drawString.split(",")) {
         val partMatches = Regex("([0-9]+) ([a-z]+)").find(segment.trim())
@@ -44,5 +64,5 @@ fun parseDraw(drawString: String): Draw {
         val (amount, colour) = partMatches.destructured
         colours[colour] = amount.toInt()
     }
-    return Draw(colours.getValue("red"), colours.getValue("green"), colours.getValue("blue"))
+    return BallSet(colours.getValue("red"), colours.getValue("green"), colours.getValue("blue"))
 }
