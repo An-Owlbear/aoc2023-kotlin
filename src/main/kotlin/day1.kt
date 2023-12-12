9a7b212da48cb383757eaa715f6fb7e5b3f91c02
AnOwlbear <anowlbear@protonmail.com>
2023-12-12 19:36:21 +0000

    day 1 second half, likely needs refactoring

--- src/main/kotlin/day1.kt
+++ src/main/kotlin/day1.kt
@@ -1,5 +1,17 @@
 package com.anowlbear
 
+val numbers = mapOf(
+    "one" to "1",
+    "two" to "2",
+    "three" to "3",
+    "four" to "4",
+    "five" to "5",
+    "six" to "6",
+    "seven" to "7",
+    "eight" to "8",
+    "nine" to "9"
+)
+
 fun main() {
     val input = object {}.javaClass.getResourceAsStream("/day1.txt")?.bufferedReader()?.readLines()
     val result = input?.sumOf { line -> getFirstLastSum(line) }
@@ -7,7 +19,30 @@ fun main() {
 }
 
 fun getFirstLastSum(line: String): Int {
-    val pattern = Regex("[0-9]")
-    val numbers = pattern.findAll(line)
-    return (numbers.first().value + numbers.last().value).toInt()
+    val digits = getAsDigits(line)
+    return (digits.first() + digits.last()).toInt()
+}
+
+fun getAsDigits(input: String): List<String> {
+    if (input.isEmpty()) return emptyList()
+    else {
+        val returnList = mutableListOf<String>()
+
+        if (input[0] in '1'..'9')
+            returnList.addLast(input[0].toString())
+        else {
+            var foundMatch = false
+            val keyIterator = numbers.keys.iterator()
+            while (!foundMatch && keyIterator.hasNext()) {
+                val key = keyIterator.next()
+                if (input.startsWith(key)) {
+                    foundMatch = true
+                    returnList.addLast(numbers[key])
+                }
+            }
+        }
+
+        returnList += getAsDigits(input.substring(1))
+        return returnList
+    }
 }
